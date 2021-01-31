15d948673aad80e2711996f4b3dede945db5b35b
hazemkhairy <hazemkhairy188@gmail.com>
2021-02-01 01:51:24 +0200

    - Changed port number.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const routes = require('./app/routes.js')
 
 const app = express();
-const port = process.env.port || 3000;
+const port = 4000 || process.env.port;
 
 
 app.use(bodyParser.json());
