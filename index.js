19959592bfeb66f3f21f8fb129254d7ce246549d
hazemkhairy <hazemkhairy188@gmail.com>
2021-02-01 23:08:45 +0200

    - Changed Port configurations. - Added start script.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const routes = require('./app/routes.js')
 
 const app = express();
-const port = 4000 || process.env.port;
+const port = process.env.PORT || 4000;
 
 
 app.use(bodyParser.json());
